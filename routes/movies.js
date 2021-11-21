b227dc89166bfe5fcd42b87a364690bcb71f3e5b
Alex Blinov <cspp43@yandex.ru>
2021-11-21 19:00:35 +0300

    feature: add validation celebrate

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const validator = require('validator');
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getMovies,
   addMovie,
@@ -6,7 +9,43 @@ const {
 } = require('../controllers/movies');
 
 router.get('/', getMovies);
-router.post('/', addMovie);
-router.delete('/:movieId', deleteMovie);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().min(2).required(),
+    director: Joi.string().min(2).required(),
+    duration: Joi.number().required(),
+    year: Joi.number().min(4).max(4).required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom((value, helper) => {
+      if (validator.isURL(value, { require_protocol: true })) {
+        return value;
+      }
+
+      return helper.message('Введите валидный url, например: https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png');
+    }),
+    trailer: Joi.string().required().custom((value, helper) => {
+      if (validator.isURL(value, { require_protocol: true })) {
+        return value;
+      }
+
+      return helper.message('Введите валидный url, например: https://www.youtube.com/watch?v=_v4jZTPknI0');
+    }),
+    thumbnail: Joi.string().required().custom((value, helper) => {
+      if (validator.isURL(value, { require_protocol: true })) {
+        return value;
+      }
+
+      return helper.message('Введите валидный url, например: https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png');
+    }),
+    movieId: Joi.string().length(24).hex().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), addMovie);
+router.delete('/:movieId', celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteMovie);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getProfile,
   updateProfile,
 } = require('../controllers/users');
 
 router.get('/me', getProfile);
-router.patch('/me', updateProfile);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+  }),
+}), updateProfile);
 
 module.exports = router;
