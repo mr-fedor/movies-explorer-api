a1df7a0e34167c995e8faf55d70b8f91535f27ac
Alex Blinov <cspp43@yandex.ru>
2021-11-25 22:27:32 +0300

    fix: add prefix /api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 
-router.use('/', require('./users'));
-router.use('/movies', auth, require('./movies'));
+router.use('/api', require('./users'));
+router.use('/api/movies', auth, require('./movies'));
 
 router.use((req, res, next) => {
   const error = new Error('Страница не найдена');
